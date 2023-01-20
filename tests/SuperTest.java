3ef0500e88c579fa2a4a7388e2add06f17027ea7
Sergey-Notebook <3173677-Sergeydigl3@users.noreply.gitlab.com>
2023-01-21 01:25:26 +0300

    Test files

---
+++ tests/SuperTest.java
@@ -0,0 +1,5 @@
+class SuperTest {
+    public static void main(String[] args) {
+        System.out.println("Hello, Worl Test!");
+    }
+}
---
+++ tests/TestBool.java
@@ -0,0 +1,7 @@
+class TestBool {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        boolean c;
+    }
+}
